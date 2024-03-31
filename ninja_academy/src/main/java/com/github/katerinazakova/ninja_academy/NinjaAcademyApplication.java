453c29c3ff707041d251f9f2cac1c9044f85a77e
katerinazakova <zakova.kz@gmail.com>
2024-03-31 14:31:46 +0200

    feat: base project ninja_academy

---
+++ ninja_academy/src/main/java/com/github/katerinazakova/ninja_academy/NinjaAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.github.katerinazakova.ninja_academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaAcademyApplication.class, args);
+	}
+
+}
