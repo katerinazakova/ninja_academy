8eccecbdbbd740cb665115eb3ff174201b4967a9
katerinazakova <zakova.kz@gmail.com>
2024-04-03 15:31:26 +0200

    feat: create new database dates and entity Dates

---
+++ src/main/java/com/github/katerinazakova/ninja_academy/entity/Dates.java
@@ -0,0 +1,39 @@
+package com.github.katerinazakova.ninja_academy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalTime;
+import java.util.EnumSet;
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+public class Dates {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(targetClass = CourseEnum.class)
+    private EnumSet<CourseEnum> nameCourse = EnumSet.allOf(CourseEnum.class);
+    @Enumerated(EnumType.STRING)
+    private DayEnum dayCourse;
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime timeCourseFrom;
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime timeCourseTo;
+    private int ageFrom;
+    private int ageTo;
+    @ManyToOne
+    private Course course;
+
+    @OneToMany (mappedBy = "date")
+    private List<Cadet> cadets;
+
+}
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,31 +1,39 @@
-
 CREATE TABLE course
 (
     id             INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name_Course    VARCHAR(20) CHECK ( name_Course IN ('TAEKWONDO', 'KICKBOX', 'GYMNASTIKA', 'KRAVMAGA') ) NOT NULL,
+    name_Course    VARCHAR(20) CHECK (name_Course IN ('TAEKWONDO', 'KICKBOX', 'GYMNASTIKA', 'KRAVMAGA') ) NOT NULL,
     characteristic TEXT,
-    coach          VARCHAR(50),
-    day_Of_Course  VARCHAR(2) CHECK (day_Of_Course IN ('PO', 'UT', 'ST', 'CT', 'PA', 'SO', 'NE'))          NOT NULL,
-    time_Of_Course TIME
+    coach          VARCHAR(50)                                                                            NOT NULL
+);
 
+CREATE TABLE dates
+(
+    id               INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name_Course      VARCHAR(20) CHECK (name_Course IN ('TAEKWONDO', 'KICKBOX', 'GYMNASTIKA', 'KRAVMAGA') ) NOT NULL,
+    day_Course       VARCHAR(2) CHECK (day_Course IN ('PO', 'UT', 'ST', 'CT', 'PA', 'SO', 'NE'))            NOT NULL,
+    time_Course_From TIME                                                                                   NOT NULL,
+    time_Course_To   TIME                                                                                   NOT NULL,
+    age_From         INT                                                                                    NOT NULL,
+    age_To           INT                                                                                    NOT NULL,
+    course_id        INT                                                                                    NOT NULL,
+    FOREIGN KEY (course_id) REFERENCES course (id)
 );
 
 
 CREATE TABLE cadet
 (
-    id             INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    first_Name     VARCHAR(50)                                                                             NOT NULL,
-    second_Name    VARCHAR(50)                                                                             NOT NULL,
-    birth_Day      DATE                                                                                    NOT NULL,
-    name_Course    VARCHAR(20) CHECK (name_Course IN ('TAEKWONDO', 'KICKBOX', 'GYMNASTIKA', 'KRAVMAGA') ) NOT NULL,
-    day_Of_Course  VARCHAR(2) CHECK (day_Of_Course IN ('PO', 'UT', 'ST', 'CT', 'PA', 'SO', 'NE'))          NOT NULL,
-    time_Of_Course TIME                                                                                    NOT NULL,
-    name_Parent    VARCHAR(50)                                                                             NOT NULL,
-    email          VARCHAR(50)                                                                             NOT NULL,
-    phone_Number   VARCHAR(15)                                                                             NOT NULL,
-    parent_Escort  BOOLEAN                                                                                 NOT NULL,
-    id_course      INT                                                                                 NOT NULL,
-    FOREIGN KEY (id_course) REFERENCES course (id)
+    id            INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_Name    VARCHAR(50)                                                                            NOT NULL,
+    second_Name   VARCHAR(50)                                                                            NOT NULL,
+    birth_Day     DATE                                                                                   NOT NULL,
+    name_Course   VARCHAR(20) CHECK (name_Course IN ('TAEKWONDO', 'KICKBOX', 'GYMNASTIKA', 'KRAVMAGA') ) NOT NULL,
+    date_Course   VARCHAR(50)                                                                            NOT NULL,
+    name_Parent   VARCHAR(50)                                                                            NOT NULL,
+    email         VARCHAR(50)                                                                            NOT NULL,
+    phone_Number  VARCHAR(15)                                                                            NOT NULL,
+    parent_Escort BOOLEAN                                                                                NOT NULL,
+    date_id      INT                                                                                    NOT NULL,
+    FOREIGN KEY (date_id) REFERENCES dates (id)
 
 );
 
