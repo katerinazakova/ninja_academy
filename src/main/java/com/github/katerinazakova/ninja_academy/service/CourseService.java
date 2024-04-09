f30610e2ef8136e6df1f84fe3e1fb3662faf30a1
katerinazakova <zakova.kz@gmail.com>
2024-04-09 17:43:28 +0200

    refactor: input ResponseStatusException into methods

--- src/main/java/com/github/katerinazakova/ninja_academy/service/CourseService.java
+++ src/main/java/com/github/katerinazakova/ninja_academy/service/CourseService.java
@@ -4,7 +4,9 @@ import com.github.katerinazakova.ninja_academy.entity.Course;
 import com.github.katerinazakova.ninja_academy.entity.Dates;
 import com.github.katerinazakova.ninja_academy.repository.CourseRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,11 +21,14 @@ public class CourseService {
     }
 
     public Course findCourseById(int id) {
-        return courseRepository.findById(id).get();
+
+        return courseRepository.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     public List<Dates> getDatesById(int id) {
-        return courseRepository.findById(id).get().getDates();
+        return courseRepository.findById(id)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND)).getDates();
     }
 
 
--- src/main/java/com/github/katerinazakova/ninja_academy/service/DatesService.java
+++ src/main/java/com/github/katerinazakova/ninja_academy/service/DatesService.java
@@ -4,7 +4,10 @@ import com.github.katerinazakova.ninja_academy.entity.Cadet;
 import com.github.katerinazakova.ninja_academy.entity.Dates;
 import com.github.katerinazakova.ninja_academy.repository.DatesRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,14 +16,15 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class DatesService {
     private final DatesRepository datesRepository;
-    public List<Dates> findAllDates (){
-        return (List<Dates>) datesRepository.findAll();
-    }
+
     public Dates findDateById (int id){
-        return datesRepository.findById(id).get();
+        return datesRepository.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
+
     public List<Cadet> getAllCadetsById (int id){
-        return datesRepository.findById(id).get().getCadets();
+        return datesRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)).getCadets();
 
     }
 }
