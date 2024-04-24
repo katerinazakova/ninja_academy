159b0e440d6042b58ce4bfa7111a67fff8ce062b
katerinazakova <zakova.kz@gmail.com>
2024-04-24 22:40:46 +0200

    refactor: add logger into methods findDateById and findCourseById

--- src/main/java/com/github/katerinazakova/ninja_academy/service/CourseService.java
+++ src/main/java/com/github/katerinazakova/ninja_academy/service/CourseService.java
@@ -4,6 +4,8 @@ import com.github.katerinazakova.ninja_academy.entity.Course;
 import com.github.katerinazakova.ninja_academy.entity.Dates;
 import com.github.katerinazakova.ninja_academy.repository.CourseRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -15,20 +17,27 @@ import java.util.Optional;
 @Service
 public class CourseService {
     private final CourseRepository courseRepository;
+    private final Logger logger = LoggerFactory.getLogger(CourseService.class);
 
     public List<Course> findAll() {
         return (List<Course>) courseRepository.findAll();
     }
 
     public Course findCourseById(int id) {
+        Optional<Course> course = courseRepository.findById(id);
+        if (course.isPresent()) {
+            return course.get();
+        } else {
+            logger.warn("kurz s id {} nebyl nalezen", id);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
 
-        return courseRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     public List<Dates> getDatesById(int id) {
-        return courseRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)).getDates();
+       return courseRepository.findById(id)
+               .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND)).getDates();
+
     }
 
 }
--- src/main/java/com/github/katerinazakova/ninja_academy/service/DatesService.java
+++ src/main/java/com/github/katerinazakova/ninja_academy/service/DatesService.java
@@ -4,6 +4,8 @@ import com.github.katerinazakova.ninja_academy.entity.Cadet;
 import com.github.katerinazakova.ninja_academy.entity.Dates;
 import com.github.katerinazakova.ninja_academy.repository.DatesRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,10 +18,16 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class DatesService {
     private final DatesRepository datesRepository;
+    private final Logger logger = LoggerFactory.getLogger(DatesService.class);
 
     public Dates findDateById (int id){
-        return datesRepository.findById(id)
-                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+       Optional<Dates> date = datesRepository.findById(id);
+       if(date.isPresent()){
+           return date.get();
+       } else {
+           logger.warn("termín s id{} není nalezen",id);
+           throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+       }
     }
 
     public List<Cadet> getAllCadetsById (int id){
